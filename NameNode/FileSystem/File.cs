04d99ceb871c91fd4e0afbb3e4e1c37f18967f75
herrbSU <herrb@seattleu.edu>
2018-02-17 17:17:14 -0800

    Basis for a file system in place, along with tests in NameNode.Program.cs

---
+++ NameNode/FileSystem/File.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+namespace NameNode.FileSystem
+{
+    public class File
+    {
+        public Dictionary<Guid, BlockNode> data { get; set; }
+        public DateTime modifiedTime { get; set; }
+        public string name { get; set; }
+        public File()
+        {
+            data = new Dictionary<Guid, BlockNode>();
+            modifiedTime = DateTime.UtcNow;
+        }
+        public File(string name)
+        {
+            this.name = name;
+            data = new Dictionary<Guid, BlockNode>();
+            modifiedTime = DateTime.UtcNow;
+        }
+    }
+}
