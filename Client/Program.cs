154b2a93e21d8b7848493f5c7694c329cd8d0444
Tong Xu <shutong.luo@gmail.com>
2018-03-03 12:07:01 -0800

    Update Program.cs

--- Client/Program.cs
+++ Client/Program.cs
@@ -17,77 +17,78 @@ namespace Client
             //// Assume passed in parameters:
             //// NameNodeIP:Port => args[0],
             // validate number of argumens
-			if (args.Length < 2)
-			{
-				System.Console.WriteLine("Please enter 2 arguments.");
-				System.Console.WriteLine("Usage: Client <action> <path>");
-			}
+		/*API between Client and NameNode*/
+		if (args.Length < 2)
+		{
+			System.Console.WriteLine("Please enter 2 arguments.");
+			System.Console.WriteLine("Usage: Client <action> <path>");
+		}
 
-			string action = args[0];
-			string path = args[1];
-			
+		string action = args[0];
+		string path = args[1];
 
-			// get connected to server
-			Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
-			var client = new ClientProto.ClientProto.ClientProtoClient(channel);
 
-			// judge what action is taken
-			if (action == "DeleteDirectory")
-			{
-				var reply = client.DeleteDirectory(new ClientProto.Path { Fullpath = path });
-				Console.WriteLine("Delete directory action: " + reply);
-			}
+		// get connected to server
+		Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+		var client = new ClientProto.ClientProto.ClientProtoClient(channel);
 
-			else if (action == "CreateDirectory")
-			{
-				var reply = client.CreateDirectory(new ClientProto.Path { Fullpath = path });
-				Console.WriteLine("Create directory action: " + reply);
-			}
+		// judge what action is taken
+		if (action == "DeleteDirectory")
+		{
+			var reply = client.DeleteDirectory(new ClientProto.Path { Fullpath = path });
+			Console.WriteLine("Delete directory action: " + reply);
+		}
 
-			else if (action == "AddFile")
-			{
-				string address = args[2];
-				var reply = client.AddFile(new ClientProto.NewFile { Fullpath = path, Address = address });
-				Console.WriteLine("Add file action: " + reply);
-			}
+		else if (action == "CreateDirectory")
+		{
+			var reply = client.CreateDirectory(new ClientProto.Path { Fullpath = path });
+			Console.WriteLine("Create directory action: " + reply);
+		}
 
-			else if (action == "DeleteFile")
-			{
-				var reply = client.DeleteFile(new ClientProto.Path { Fullpath = path });
-				Console.WriteLine("Delete file action: " + reply);
-			}
+		else if (action == "AddFile")
+		{
+			string address = args[2];
+			var reply = client.AddFile(new ClientProto.NewFile { Fullpath = path, Address = address });
+			Console.WriteLine("Add file action: " + reply);
+		}
 
-			else if (action == "TestFile")
-			{
-				var reply = client.TestFile(new ClientProto.Path { Fullpath = path });
-				Console.WriteLine("Test file action: " + reply);
-			}
+		else if (action == "DeleteFile")
+		{
+			var reply = client.DeleteFile(new ClientProto.Path { Fullpath = path });
+			Console.WriteLine("Delete file action: " + reply);
+		}
 
-			else if (action == "ReadFile")
-			{
-				var reply = client.ReadFile(new ClientProto.Path { Fullpath = path });
+		else if (action == "TestFile")
+		{
+			var reply = client.TestFile(new ClientProto.Path { Fullpath = path });
+			Console.WriteLine("Test file action: " + reply);
+		}
 
-				Console.WriteLine("Read file action: " + reply);
-			}
+		else if (action == "ReadFile")
+		{
+			var reply = client.ReadFile(new ClientProto.Path { Fullpath = path });
 
-			else if (action == "RenameFile")
-			{
-				var reply = client.RenameFile(new ClientProto.Path { Fullpath = path });
-				Console.WriteLine("Rename file action: " + reply);
-			}
+			Console.WriteLine("Read file action: " + reply);
+		}
 
-			else if (action == "MoveFile")
-			{
-				string newpath = args[2];
-				var reply = client.MoveFile(new ClientProto.DoublePath { Fullpath = path, Newpath = newpath });
-				Console.WriteLine("Move file action: " + reply);
-			}
+		else if (action == "RenameFile")
+		{
+			var reply = client.RenameFile(new ClientProto.Path { Fullpath = path });
+			Console.WriteLine("Rename file action: " + reply);
+		}
 
-			channel.ShutdownAsync().Wait();
+		else if (action == "MoveFile")
+		{
+			string newpath = args[2];
+			var reply = client.MoveFile(new ClientProto.DoublePath { Fullpath = path, Newpath = newpath });
+			Console.WriteLine("Move file action: " + reply);
+		}
 
-			Console.WriteLine("Press any key to exit...");
-			Console.ReadKey();
-            
+		channel.ShutdownAsync().Wait();
+
+		Console.WriteLine("Press any key to exit...");
+		Console.ReadKey();
+
             
             
             
