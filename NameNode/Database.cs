99809e11209ebf223487980450d7436afa2f3540
herrbSU <herrb@seattleu.edu>
2018-03-11 15:22:35 -0700

    * ClientProto.proto: Added more StatusTypes to more messages
    * Database.cs: database no longer returns null
    unless it is a freak accident with addBlockToFile

--- NameNode/Database.cs
+++ NameNode/Database.cs
@@ -124,16 +124,26 @@ namespace NameNode
                         Console.Write("ip: " + a);
                     }
                     Console.WriteLine();
-                    blockInfos.Add(new ClientProto.BlockInfo { BlockId = new ClientProto.UUID { Value = blockID.ToString() }, BlockSize = Constants.MaxBlockSize, IpAddress = { ipAddresses } });
+                    blockInfos.Add(new ClientProto.BlockInfo { 
+                        BlockId = new ClientProto.UUID { Value = blockID.ToString() },
+                        BlockSize = Constants.MaxBlockSize,
+                        IpAddress = { ipAddresses } });
                 }
 
                 //send back to client the ips and what to search for on the datanode
-                return new ClientProto.BlockMessage { BlockInfo = { blockInfos } , FileSize = toBeRead.fileSize };
+                return new ClientProto.BlockMessage { 
+                    BlockInfo = { blockInfos } , 
+                    FileSize = toBeRead.fileSize,
+                    Type = ClientProto.BlockMessage.Types.StatusType.Success
+                };
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-                return null;
+                return new ClientProto.BlockMessage { 
+                    BlockInfo = { new List<ClientProto.BlockInfo>() }, 
+                    FileSize = 0, 
+                    Type = ClientProto.BlockMessage.Types.StatusType.Fail };
             }
         }
 
